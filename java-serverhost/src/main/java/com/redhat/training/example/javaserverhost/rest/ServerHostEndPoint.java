12ef13357597688335969b43ce0d87e4d6b2f806
Student User <student@workstation.lab.example.com>
2020-04-01 17:32:45 +0000

    Update the app name to green

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -19,7 +19,7 @@ public class ServerHostEndPoint {
     catch (Exception e) {
        e.printStackTrace();
     }
-    String msg = "Red App running on server "+host+" Version 1.0 \n";
+    String msg = "Green App running on server "+host+" Version 1.0 \n";
     return Response.ok(msg).build();
   }
 }
